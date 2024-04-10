fa6cfbcd351357b1eee216900754ba988f4f1e62
Mohammed Rami <mohammed.rami@innovaturelabs.com>
2024-04-10 08:18:32 +0530

    new changes

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -3,7 +3,7 @@ export const commonStyles = [
   '../../assets/lib/animate/animate.min.css',
   '../../assets/lib/owlcarousel/assets/owl.carousel.min.css',
   '../../assets/lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css',
-  '../../assets/css/bootstrap.min.css',
+
   '../../assets/css/style.css',
 ];
 export const ITEMS = [

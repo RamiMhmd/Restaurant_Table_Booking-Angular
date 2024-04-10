2220a2561b2c74c59d33eaddd044fba5a1661818
Mohammed Rami <mohammed.rami@innovaturelabs.com>
2024-04-10 10:24:02 +0530

    new changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css',
-  '../assets/lib/animate/animate.min.css',
   '../assets/lib/owlcarousel/assets/owl.carousel.min.css',
-  '../assets/lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css',
   '../assets/css/bootstrap.min.css',
   '../assets/css/style.css',
   ]
